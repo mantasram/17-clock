11a9f2b479312c5dd8acfe043f101d10f6772bed
mramo21 <mantasramo@gmail.com>
2021-05-25 18:28:48 +0300

    initial commit

---
+++ js/tick.js
@@ -0,0 +1,12 @@
+const a = [2, 3, 1, 4, 2];
+let i = 0
+
+
+const timer = setInterval(() => {
+   const number = a[i++]
+    if (typeof number === 'number') {
+        console.log(number);
+    } else {
+        clearInterval(timer);
+    }
+}, 1000);
