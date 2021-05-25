3672d55968ccf29a31bbead97279a292ae76c7b0
mramo21 <mantasramo@gmail.com>
2021-05-25 18:41:27 +0300

    tack

--- js/tack.js
+++ js/tack.js
@@ -1,2 +1,9 @@
 const a = [2, 3, 1, 4, 2];
+let waitingTime = 0;
 
+for (const number of a) {
+    waitingTime += number;
+    setTimeout(() => {
+        console.log(number);
+    }, waitingTime * 1000)
+}
